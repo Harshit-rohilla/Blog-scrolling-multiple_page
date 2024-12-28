7e3dd594b31cdb557fb1fe52d4aa38b9fc63cb50
Harshit Rohilla <harshitrohilla105@gmail.com>
2024-12-28 15:09:30 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from "react"
 import Blogs from "./components/Blogs"
+import Loader from "./components/Loader"
 
 function App(){
     const[currentPage,changeCurrentPage]=useState(1)
     const[totalPage,changeTotalPage]=useState(null)
     const[data,setData]=useState([])
+    const[loading,setLoading]=useState(false)
     
     async function apiCall(){
+        setLoading(true)
         try{
             let res=await fetch(`https://codehelp-apis.vercel.app/api/get-blogs?page=${currentPage}`)
             let res2=await res.json()
             setData(res2)
-            changeTotalPage(data.totalPages)   
+            changeTotalPage(data.totalPages)
+            setLoading(false)   
         }
         catch(error){
             console.log(error)
+            setLoading(false)
         }
     }
     useEffect(()=>{
@@ -25,7 +30,7 @@ function App(){
         <>
         <div className="wrapper flex flex-col h-screen w-full">
             <div className="header w-full border-b shadow-[0_2px_10px] py-2 text-center font-bold text-2xl">MY BLOGS</div>
-            <Blogs currentPageData={data}/>
+            {loading?(<Loader/>):(<Blogs currentPageData={data}/>)}
             <div className="footer w-full py-2 flex justify-evenly border border-t-gray-300">
                 <div>
                     <button onClick={()=>{changeCurrentPage((prev)=>{if(prev>1){return prev-1}else{return prev}})}} className="py-1 px-3 border border-black mr-2">Previous</button>
